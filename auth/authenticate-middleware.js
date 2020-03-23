5cb1903f31f31015b208e4db299e451376837dd8
Joey Nevarez <josephnevarez12@gmail.com>
2020-03-22 17:41:33 -0700

    wrote authenticate middleware

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -7,12 +7,12 @@ const jwt = require("jsonwebtoken")
 
 module.exports = function authenticate() {
   return async (req, res, next) => {
-    const { authToken } = req.cookies
-    if (!authToken){
+    const { token } = req.cookies
+    if (!token){
         return res.status(400).json({message: "No credentials provided"})
     } 
 
-    jwt.verify(authToken, process.env.JWT_SECRET, (err, decodedToken) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
         if(err){
             res.status(401).json({ message: "You shall not pass!" })
         } else {
